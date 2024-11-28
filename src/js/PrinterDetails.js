57fe945a0911d4d9f5b966d65e6155c264041a11
TinnieTheCat198 <tinnie2004@gmail.com>
2024-11-28 22:55:01 +0700

    Add styling for Printer Details

--- src/js/PrinterDetails.js
+++ src/js/PrinterDetails.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import printerData from '../db/printer';
+import printerImg from '../assets/img/pngwing.com.png'
 import '../css/PrinterDetails.css';
 
 const PrinterDetails = () => {
@@ -22,32 +23,57 @@ const PrinterDetails = () => {
 
       <div className="printer-info">
         <div className="printer-main-info">
-              <p>ID: {printer.id}</p>
-              <p>Name: {printer.name}</p>
-              <p>Type: {printer.type}</p>
-              <p>Status: {printer.status}</p>
-              <p>
-                Location: {printer.location.building}, Floor{" "}
-                {printer.location.floor}
-              </p>
-              <p>
-                Paper Tray Status: {printer.paperTrayStatus.remainingPages}{" "}
-                {printer.paperTrayStatus.paperSize} pages remaining
-              </p>
-              <p>
-                Error Alerts:{" "}
-                {printer.errorAlerts ? printer.errorAlerts : "None"}
-              </p>
-              <p>
-                Description:{" "}
+        <table>
+          <tr>
+            <th>ID:</th>
+            <td>{printer.id}</td>
+          </tr>
+          <tr>
+            <th>Name:</th>
+            <td id="printer-name">{printer.name}</td>
+          </tr>
+          <tr>
+            <th>Status:</th>
+            <td id="printer-status" className={printer.status}>{printer.status}</td>
+          </tr>
+          <tr>
+            <th>Type:</th>
+            <td>{printer.type}</td>
+          </tr>
+          <tr>
+            <th>Location:</th>
+            <td>{printer.location.building}, Floor {printer.location.floor}</td>
+          </tr>
+          <tr>
+            <th>Paper Tray Status:</th>
+            <td><span id="paper-remain">{printer.paperTrayStatus.remainingPages}</span>{" "}
+            {printer.paperTrayStatus.paperSize} pages remaining</td>
+          </tr>
+          <tr>
+            <th>Error Alerts:</th>
+            <td id="printer-error" className={`${printer.errorAlerts ? "error-text" : ""}`}>
+              {printer.errorAlerts ? printer.errorAlerts : "None"}
+            </td>
+          </tr>
+          <tr>
+            <th>Description:</th>
+            <td className={`${printer.description ? "" : "empty-description"}`}>
                 {printer.description
                   ? printer.description
-                  : "No description available"}
-              </p>
-              <p>IPv4 Address: {printer.network.ipv4Address}</p>
-              <p>Connection Speed: {printer.network.connectionSpeed}</p>
-        </div>
+                  : "No description available"}</td>
+          </tr>
+          <tr>
+            <th>IPv4 address:</th>
+            <td>{printer.network.ipv4Address}</td>
+          </tr>
+          <tr>
+            <th>Connection Speed:</th>
+            <td>{printer.network.connectionSpeed}</td>
+          </tr>
+        </table>
         
+        </div>
+        <img id="printer-img" src={printerImg}></img>
       </div>
       <div className="center-button">
           <button className="disable-printer-btn">Disable Printer</button>
